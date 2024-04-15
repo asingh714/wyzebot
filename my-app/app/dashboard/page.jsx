2470612b7ae8faf3e373450a15cb65c8ed9193c2
Aman <amsingh714@gmail.com>
2024-04-15 14:29:59 -0400

    created dashboard with code mirrors and styling for the dashboard page

---
+++ my-app/app/dashboard/page.jsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { javascript } from "@codemirror/lang-javascript";
+import { html } from "@codemirror/lang-html";
+import { css } from "@codemirror/lang-css";
+import CodeMirror from "@uiw/react-codemirror";
+import "./dashboard.css";
+
+const DashboardPage = () => {
+  const [html_edit, setHtmlEdit] = useState("");
+  const [css_edit, setCssEdit] = useState("");
+  const [js_edit, setJsEdit] = useState("");
+
+  return (
+    <div className="dashboard-grid-container">
+      <div className="code-mirrors-container">
+        <div className="code-mirror-bg">
+          <h2 className="code-mirror-heading">HTML</h2>
+          <CodeMirror
+            className="codeMirror"
+            value={html_edit}
+            theme="dark"
+            extensions={[html(true)]}
+          />
+        </div>
+        <div className="code-mirror-bg">
+          <h2 className="code-mirror-heading">CSS</h2>
+          <CodeMirror
+            className="codeMirror"
+            value={css_edit}
+            theme="dark"
+            extensions={[css(true)]}
+          />
+        </div>
+        <div className="code-mirror-bg">
+          <h2 className="code-mirror-heading">JavaScript</h2>
+          <CodeMirror
+            className="codeMirror"
+            value={js_edit}
+            theme="dark"
+            extensions={[javascript(true)]}
+          />
+        </div>
+      </div>
+      <div className="result-form-container">
+        <div className="result">RESULT</div>
+        <form onSubmit={() => {}}>
+          <textarea type="text" />
+          <button type="submit">Submit Code</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
