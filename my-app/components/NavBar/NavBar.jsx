7d7f66ea680325b72500d859ec3e900d732ad59a
Aman <amsingh714@gmail.com>
2024-05-03 14:26:03 -0400

    fixed navbar index bug

--- my-app/components/NavBar/NavBar.jsx
+++ my-app/components/NavBar/NavBar.jsx
@@ -25,13 +25,13 @@ const NavBar = () => {
           <div className="z-50">
             <Link
               href="/dashboard"
-              className="text-2xl hover:text-gray-300 mr-4"
+              className="text-2xl hover:text-gray-300 mr-4 z-50 "
             >
               Dashboard
             </Link>
             <button
               onClick={() => signOut()}
-              className=" text-white text-2xl hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 transition duration-300 ease-in-out"
+              className=" text-white text-2xl hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 transition duration-300 ease-in-out z-50"
             >
               Sign Out
             </button>
@@ -43,7 +43,7 @@ const NavBar = () => {
             <button
               onClick={() => signIn(provider.id)}
               key={index}
-              className="flex items-center text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 z-50 transition duration-300 ease-in-out"
+              className="flex items-center text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 z-50 transition duration-300 ease-in-out z-50"
             >
               <FaGoogle className="fa-brands fa-google text-white mr-2"></FaGoogle>
               <span>Login or Register</span>
