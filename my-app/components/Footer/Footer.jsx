8f41f8328c480617eca1b54cf53bb889d5c2aa5b
Aman <amsingh714@gmail.com>
2024-05-06 13:42:50 -0400

    updated footer for mobile screens

--- my-app/components/Footer/Footer.jsx
+++ my-app/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="bg-black text-white p-16 flex justify-center items-center">
-      <div className="flex justify-between items-center max-w-[900px] w-full">
-        <h1 className="text-3xl font-bold">WyzeBot</h1>
+      <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center max-w-[900px] w-full">
+        <h1 className="text-3xl font-bold mb-4 sm:mb-0">WyzeBot</h1>
         <div>© 2024 WyzeBot. All rights reserved.</div>
       </div>
     </footer>
--- my-app/components/Home/Features.jsx
+++ my-app/components/Home/Features.jsx
@@ -2,11 +2,10 @@ import { HoverEffect } from "@/components/ui/HoverEffect";
 
 export default function Features() {
   return (
-    <div className="h-[75rem] w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-center">
+    <div className="sm:h-[100rem] h-[75rem] w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-
-      <div className="max-w-[100rem] mx-auto px-2">
-        <h2 className="text-6xl font-semibold text-white text-center">
+      <div className="max-w-[100rem] mx-auto px-2 ">
+        <h2 className="text-6xl font-semibold text-white text-center sm:text-black">
           What Wyzebot Can Do
         </h2>
         <HoverEffect items={features} />
